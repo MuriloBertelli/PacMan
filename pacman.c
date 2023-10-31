0c15fb21ad7dd474068d272ca9829c6deb6ff5c2
MuriloBertelli <mrlbertelli@gmail.com>
2023-10-31 15:11:43 -0300

    Update pacman.c

--- pacman.c
+++ pacman.c
@@ -1,26 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pacman.h"
+
+char **mapa;
+int linhas;
+int colunas;
+
+void lemapa()
+{
+	FILE *f;
+	f = fopen("mapa.txt", "r");
+	if (f == 0)
+	{
+		printf("Erro na leitura do mapa");
+		exit(1);
+	}
+
+	fscanf(f, "%d %d", &linhas, &colunas);
+	alocamapa();
+
+	for (int i = 0; i < 5; i++)
+	{
+		fscanf(f, "%s", mapa[i]);
+	}
+
+	fclose(f);
+}
+
+void alocamapa()
+{
+	mapa = malloc(sizeof(char *) * linhas);
+
+	for (int i = 0; i < linhas; i++)
+	{
+		mapa[i] = malloc(sizeof(char) * colunas + 1);
+	}
+}
+
+void liberamapa()
+{
+	for (int i = 0; i < linhas; i++)
+	{
+		free(mapa[i]);
+	}
+
+	free(mapa);
+}
 
 int main()
 {
-    char mapa[5][10 + 1];
 
-    FILE *f;
-    f = fopen("mapa.txt", "r");
+	lemapa();
 
-    if (f == 0)
-    {
-        printf("Erro na leitura do mapa\n");
-        exit(1);
-    }
-    for (int i = 0; i < 5; i++)
-    {
-        fscanf(f, "%s", mapa[i]);
-    }
+	for (int i = 0; i < linhas; i++)
+	{
+		printf("%s\n", mapa[i]);
+	}
 
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%s\n", mapa[i]);
-    }
-    fclose(f);
+	liberamapa();
 }
