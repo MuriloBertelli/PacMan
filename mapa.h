dbde5b190503eb5b1416766d0c3be72deeedbf00
MuriloBertelli <mrlbertelli@gmail.com>
2023-11-07 17:36:04 -0300

    Create mapa.h

---
+++ mapa.h
@@ -0,0 +1,13 @@
+struct mapa
+{
+    char **matriz;
+    int linhas;
+    int colunas;
+};
+
+typedef struct mapa MAPA;
+
+void alocamapa(MAPA *m);
+void lemapa(MAPA *m);
+void liberamapa(MAPA *m);
+void imprimemapa(MAPA *m);
