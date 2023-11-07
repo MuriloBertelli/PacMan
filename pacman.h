1d6f86f2db0050c3cb0e2dd39a62df35838312cf
MuriloBertelli <mrlbertelli@gmail.com>
2023-11-07 17:36:12 -0300

    Update pacman.h

--- pacman.h
+++ pacman.h
@@ -1,16 +1,3 @@
 
-void alocamapa();
-void lemapa();
-void liberamapa();
 void move(char direcao);
 int acabou();
-void imprimemapa();
-
-struct mapa
-{
-    char **matriz;
-    int linhas;
-    int colunas;
-};
-
-typedef struct mapa MAPA;
