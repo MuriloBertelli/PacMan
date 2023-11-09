3d27cb7616f356aba2bab58a8b08add1ad916f21
MuriloBertelli <mrlbertelli@gmail.com>
2023-11-09 15:36:07 -0300

    Update mapa.h

--- mapa.h
+++ mapa.h
@@ -7,7 +7,16 @@ struct mapa
 
 typedef struct mapa MAPA;
 
+struct posicao
+{
+    int x;
+    int y;
+};
+
+typedef struct posicao POSICAO;
+
 void alocamapa(MAPA *m);
 void lemapa(MAPA *m);
 void liberamapa(MAPA *m);
 void imprimemapa(MAPA *m);
+void encontramapa(MAPA *m, POSICAO *p, char c);
