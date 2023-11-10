e214e426c19a163d18768527825ed4284b28b61e
MuriloBertelli <mrlbertelli@gmail.com>
2023-11-10 17:31:54 -0300

    Update pacman.c

--- pacman.c
+++ pacman.c
@@ -2,48 +2,85 @@
 #include <stdlib.h>
 #include "pacman.h"
 #include "mapa.h"
-
 MAPA m;
 POSICAO heroi;
 
-acabou()
+void fantasmas()
+{
+
+	MAPA copia;
+	copiamapa(&copia, &m);
+
+	for (int i = 0; i < m.linhas; i++)
+	{
+		for (int j = 0; j < m.colunas; j++)
+		{
+			if (copia.matriz[i][j] == FANTASMA)
+			{
+				if (ehvalida(&m, i, j + 1) && ehvazia(&m, i, j + 1))
+				{
+					andanomapa(&m, i, j, i, j + 1);
+				}
+			}
+		}
+	}
+	liberamapa(&copia);
+}
+
+int acabou()
 {
 	return 0;
 }
 
+int ehdirecao(char direcao)
+{
+	return direcao == ESQUERDA ||
+		   direcao == CIMA ||
+		   direcao == BAIXO ||
+		   direcao == DIREITA;
+}
+
 void move(char direcao)
 {
-	int x;
-	int y;
 
-	m.matriz[heroi.x][heroi.y] = '.';
+	if (!ehdirecao(direcao))
+		return;
+
+	int proximox = heroi.x;
+	int proximoy = heroi.y;
 
 	switch (direcao)
 	{
-	case 'a':
-		m.matriz[heroi.x][heroi.y - 1] = '@';
-		heroi.y--;
+	case ESQUERDA:
+		proximoy--;
 		break;
-	case 'w':
-		m.matriz[heroi.x - 1][heroi.y] = '@';
-		heroi.x--;
+	case CIMA:
+		proximox--;
 		break;
-	case 's':
-		m.matriz[heroi.x + 1][heroi.y] = '@';
-		heroi.x++;
+	case BAIXO:
+		proximox++;
 		break;
-	case 'd':
-		m.matriz[heroi.x][heroi.y + 1] = '@';
-		heroi.y++;
+	case DIREITA:
+		proximoy++;
 		break;
 	}
+
+	if (!ehvalida(&m, proximox, proximoy))
+		return;
+
+	if (!ehvazia(&m, proximox, proximoy))
+		return;
+
+	andanomapa(&m, heroi.x, heroi.y, proximox, proximoy);
+	heroi.x = proximox;
+	heroi.y = proximoy;
 }
 
 int main()
 {
 
 	lemapa(&m);
-	encontramapa(&m, &heroi, '@');
+	encontramapa(&m, &heroi, HEROI);
 
 	do
 	{
@@ -53,6 +90,7 @@ int main()
 		scanf(" %c", &comando);
 
 		move(comando);
+		fantasmas();
 
 	} while (!acabou());
 
