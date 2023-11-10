1b3c556db972c027241cd0c944b1da61ad1ebe93
MuriloBertelli <mrlbertelli@gmail.com>
2023-11-10 17:31:49 -0300

    Update mapa.c

--- mapa.c
+++ mapa.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mapa.h"
+
+void copiamapa(MAPA *destino, MAPA *origem)
+{
+    destino->linhas = origem->linhas;
+    destino->colunas = origem->colunas;
+
+    alocamapa(destino);
+    for (int i = 0; i < origem->linhas; i++)
+    {
+        for (int j = 0; j < origem->colunas; j++)
+        {
+            strcpy(destino->matriz[i], origem->matriz[i]);
+        }
+    }
+}
+
+void andanomapa(MAPA *m, int xorigem, int yorigem,
+                int xdestino, int ydestino)
+{
+
+    char personagem = m->matriz[xorigem][yorigem];
+    m->matriz[xdestino][ydestino] = personagem;
+    m->matriz[xorigem][yorigem] = VAZIO;
+}
+
+int ehvazia(MAPA *m, int x, int y)
+{
+    return m->matriz[x][y] == VAZIO;
+}
+
+int ehvalida(MAPA *m, int x, int y)
+{
+    if (x >= m->linhas)
+        return 0;
+    if (y >= m->colunas)
+        return 0;
+    return 1;
+}
 
 void encontramapa(MAPA *m, POSICAO *p, char c)
 {
